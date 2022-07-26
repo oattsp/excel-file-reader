bc44c6eaa230535dbeb2e2ab27dd4ad61cb4b363
oattsp <thodsaphon.aiab@gmail.com>
2022-07-26 20:32:55 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import pandas
+
+resultSet = []
+
+# Folder Path
+path = "files"
+
+# Change the directory
+os.chdir(path)
+
+
+# Read xlsx file
+def count_rows_xlsx_file(file_path):
+    excel_data_df = pandas.read_excel(file_path)
+    return len(excel_data_df)
+
+
+def main():
+    for file in os.listdir():
+        if file.endswith(".xlsx"):
+            file_path = os.path.join(os.path.dirname(__file__), path, file)
+            count = count_rows_xlsx_file(file_path)
+            resultSet.append({"file_name": file, "count": count})
+
+    data_frame = pandas.DataFrame(resultSet)
+    output_path = os.path.join(os.path.dirname(__file__), "output", "report.csv")
+    data_frame.to_csv(output_path)
+    data_frame.to_csv(output_path, na_rep='Unkown')  # missing value save as Unknown
+    data_frame.to_csv(output_path, float_format='%.2f')  # rounded to two decimals
+    data_frame.to_csv(output_path, index=False)
+
+
+if __name__ == '__main__':
+    main()
